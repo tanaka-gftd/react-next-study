d0ea0dc40b9a955c769d12a10d32af1853808cd7
Tanaka <f.tanaka@gftd.works>
2022-09-20 11:02:58 +0900

    練習に、国、首都、都道府県庁所在地が表示

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,21 @@
+const TopPage = () => {
+  return (
+    <div>
+      <h1>国の首都リスト</h1>
+      <div>日本 ... 東京</div>
+      <div>アメリカ ... ワシントンD.C</div>
+      <div>中国 ... 北京</div>
+      <div>英国 ... ロンドン</div>
+      <h1>都道府県庁所在地リスト</h1>
+      <div>東京 ... 東京特別区</div>
+      <div>北海道 ... 札幌市</div>
+      <div>宮城県 ... 仙台市</div>
+      <div>愛知県 ... 名古屋市</div>
+      <div>大阪府 ... 大阪市</div>
+      <div>広島県 ... 広島市</div>
+      <div>福岡県 ... 福岡市</div>
+    </div>
+  );
+};
+
+export default TopPage;
