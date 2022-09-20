89c8d312dbe34d8a84aa4973bdf7ff6fc5e7e106
Tanaka <f.tanaka@gftd.works>
2022-09-20 11:21:15 +0900

    国の首都リスト及び都道府県庁所在地リストの繰り返し部分を、配列で保持するようにした（＆都庁所在地を変更）

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,26 +1,46 @@
+const countryCapitals = [
+  {country: '日本', capital: '東京'},
+  {country: 'アメリカ', capital: 'ワシントンD.C'},
+  {country: '中国', capital: '北京'},
+  {country: 'イギリス', capital: 'ロンドン'}
+];
+
 const CountryCapitalList = () => {
   return (
     <div>
       <h1>国の首都リスト</h1>
-      <div>日本 ... 東京</div>
-      <div>アメリカ ... ワシントンD.C</div>
-      <div>中国 ... 北京</div>
-      <div>英国 ... ロンドン</div>
+      {countryCapitals.map(countryCapital => {
+        return (
+          <div key={countryCapital.country}>
+            {countryCapital.country} ... {countryCapital.capital}
+          </div>
+        );
+      })}
     </div>
   );
 };
+
+const prefectureCapitals = [
+  {prefecture: '東京都', capital: '新宿区'},
+  {prefecture: '北海道', capital: '札幌市'},
+  {prefecture: '宮城県', capital: '仙台市'},
+  {prefecture: '愛知県', capital: '名古屋市'},
+  {prefecture: '大阪府', capital: '大阪市'},
+  {prefecture: '広島県', capital: '広島市'},
+  {prefecture: '福岡県', capital: '福岡市'},
+];
 
 const PrefectureCapitalList = () => {
   return (
     <div>
       <h1>都道府県庁所在地リスト</h1>
-      <div>東京 ... 東京特別区</div>
-      <div>北海道 ... 札幌市</div>
-      <div>宮城県 ... 仙台市</div>
-      <div>愛知県 ... 名古屋市</div>
-      <div>大阪府 ... 大阪市</div>
-      <div>広島県 ... 広島市</div>
-      <div>福岡県 ... 福岡市</div>
+      {prefectureCapitals.map(prefectureCapital => {
+        return (
+        <div key={prefectureCapital.prefecture}>
+          {prefectureCapital.prefecture} ... {prefectureCapital.capital}
+        </div>
+        );
+      })}
     </div>
   );
 };
