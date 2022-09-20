1ecf058bc6fb2785a81eebba3fade096d53636de
Tanaka <f.tanaka@gftd.works>
2022-09-20 15:42:45 +0900

    PrefectureCapitaListコンポーネント内でもpropsを用いて細かく分割＆スタイルを設定

--- my-app/components/PrefectureCapitaList.tsx
+++ my-app/components/PrefectureCapitaList.tsx
@@ -1,4 +1,15 @@
-const prefectureCapitals = [
+/* 都道府県庁リストの方でも、propsを用いて細かく分割 */
+
+interface PrefectureCapitalItemProps {
+  children: React.ReactNode;
+};
+
+interface PrefectureCapitalProps {
+  prefecture: string;
+  capital: string;
+};
+
+const prefectureCapitals: PrefectureCapitalProps[] = [
   {prefecture: '東京都', capital: '新宿区'},
   {prefecture: '北海道', capital: '札幌市'},
   {prefecture: '宮城県', capital: '仙台市'},
@@ -9,15 +20,39 @@ const prefectureCapitals = [
   {prefecture: '沖縄県', capital: '那覇市'}
 ];
 
+const PrefectureCapitalItemContainer = (props:PrefectureCapitalItemProps) => {
+  return(
+    <div style={{display:"flex", marginBottom:"0.5em"}}>
+      {props.children}
+    </div>
+  ); 
+};
+
+const PrefectureCapitalItem = (props: PrefectureCapitalProps) => {
+  return (
+    <>
+      <div style={{color:"blue", fontWeight:"bold"}}>
+        {props.prefecture}&ensp;...&ensp;
+      </div>
+      <div style={{color:"green"}}> 
+        {props.capital}
+      </div>
+    </>
+  )
+}
+
 const PrefectureCapitalList = () => {
   return (
     <div>
       <h1>都道府県庁所在地リスト</h1>
       {prefectureCapitals.map(prefectureCapital => {
         return (
-        <div key={prefectureCapital.prefecture} style={{color: 'blue'}}>
-          {prefectureCapital.prefecture} ... {prefectureCapital.capital}
-        </div>
+        <PrefectureCapitalItemContainer key={prefectureCapital.prefecture}>
+          <PrefectureCapitalItem
+            prefecture={prefectureCapital.prefecture}
+            capital={prefectureCapital.capital}
+          />
+        </PrefectureCapitalItemContainer>
         );
       })}
     </div>
